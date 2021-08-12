4b6187ce23a7ee60a5705b7b9399ce4b8a210573
Stennish <68435891+Stennish@users.noreply.github.com>
2021-08-12 03:27:06 +0300

    Farmer Steve Install script

---
+++ Farmer Steve/Install.lua
@@ -0,0 +1,14 @@
+print("Installing Farmer Steve.iso")
+print("Copyright: Stennish - 2021")
+os.sleep(2.5)
+pastebin get https://pastebin.com/VXy4a8CZ init
+pastebin get https://pastebin.com/BGEgWDsd NextPlot
+pastebin get https://pastebin.com/Wdi4BKSp NextRowLeft
+pastebin get https://pastebin.com/nNMvQN7b NextRowRight
+pastebin get https://pastebin.com/MAPxNUwj PlaceSeed
+pastebin get https://pastebin.com/y0vGqhDk Refill
+pastebin get https://pastebin.com/GvpLhcDh Replant
+pastebin get https://pastebin.com/xvcrmfTs Return
+os.sleep(1)
+print("Farmer Steve Successfully Installed!")
+label set "Farmer Steve"
