0288214463b811d8e766bfb7341c69ff94bff4fb
Stennish <68435891+Stennish@users.noreply.github.com>
2021-08-12 07:15:36 +0300

    aaaa

--- Farmer Steve/Install.lua
+++ Farmer Steve/Install.lua
@@ -4,8 +4,7 @@ print("")
 os.slowPrint("Designed for: 2 8x16 Farms.")
 os.slowPrint("Image: imgur.com/ctxxMas.png")
 os.sleep(2.5)
-pastebin get 7jJ8BfR7 Replant
-pastebin get fChmY7q7 startup
+DownloadFiles()
 os.sleep(1)
 os.slowPrint("Farmer Steve Successfully Installed!")
 os.slowPrint("Enjoy your farming!")
@@ -13,3 +12,11 @@ os.sleep(1.5)
 os.slowPrint("Restarting in 5 seconds...")
 os.sleep(5)
 os.reboot()
+
+
+
+
+function DownloadFiles()
+    pastebin get 7jJ8BfR7 Replant
+    pastebin get fChmY7q7 startup
+end
