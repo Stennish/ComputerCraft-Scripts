ff72378cbded0cb3f525d12f509d6722cdc90e1b
Stennish <68435891+Stennish@users.noreply.github.com>
2021-08-12 11:54:32 +0300

    i hate this lmao

--- Farmer Steve/Replant.lua
+++ Farmer Steve/Replant.lua
@@ -1,3 +1,25 @@
+-- WIP Functions are commented out!
+
+-- local function printUsage()
+--     print( "Usage:" )
+--     print( "Replant <number of plots (maximum 2)")
+-- end
+-- 
+-- local variable = { ... }
+-- if #variable < 1 then
+--     printUsage()
+--     return
+-- elseif #variable == 1 then
+--     init()
+--     Plot1()
+-- elseif #variable == 2 then
+--     init()
+--     Plot1()
+--     Plot2()
+-- end
+-- note to self: get ^this^ goddamn garbage to work.
+
+-- main code
 init()
 print("Started!")
 for i=1,4 do -- Fill first plot
@@ -36,27 +58,26 @@ print("Done!")
 
 
 
-
-function NextPlot() -- Move onto the Next Plot
+local function NextPlot() -- Move onto the Next Plot
     turtle.turnLeft()
     turtle.forward()
     turtle.forward()
     turtle.turnRight()
 end
 
-function NextLeft() -- Go to next row on the left
+local function NextLeft() -- Go to next row on the left
     turtle.turnLeft()
     turtle.forward()
     turtle.turnLeft()
 end
 
-function NextRight() -- Go to next row on the right
+local function NextRight() -- Go to next row on the right
     turtle.turnRight()
     turtle.forward()
     turtle.turnRight()
 end
 
-function PlaceSeed() -- Place seeds and go forward.
+local function PlaceSeed() -- Place seeds and go forward.
     for i=1,16 do
         turtle.placeDown()
         turtle.forward()
@@ -64,7 +85,7 @@ function PlaceSeed() -- Place seeds and go forward.
     end
 end
 
-function Return() -- Go back to starting position.
+local function Return() -- Go back to starting position.
     turtle.turnRight()
     for i=1,18 do
         turtle.forward()
@@ -74,7 +95,59 @@ function Return() -- Go back to starting position.
     turtle.down()
 end
 
-function init() -- Prepare for Replanting.
+-- local function Return1() -- Go back to starting position. [1 Plot]
+--     turtle.turnLeft()
+--     for i=1,8 do
+--         turtle.forward()
+--     end
+--     turtle.turnLeft()
+--     turtle.back()
+--     turtle.down()
+-- end
+
+-- local function Return2() -- Go back to starting position. [2 Plots]
+--     turtle.turnRight()
+--     for i=1,18 do
+--         turtle.forward()
+--     end
+--     turtle.turnLeft()
+--     turtle.back()
+--     turtle.down()
+-- end
+
+-- local function Plot1()
+--     for i=1,4 do -- Fill first plot
+--         PlaceSeed()
+--         NextLeft()
+--         PlaceSeed()
+--         NextRight()
+--         print("Refilling Slot 1...")
+--         Refill()
+--         print("Continuing...")
+--     end
+--     print("Finished, returning to start point.")
+--     Return1()
+--     print("Done!")
+-- print("Done!")end
+-- 
+-- local function Plot2()
+--     print("First plot done, moving on to next...")
+--     NextPlot()
+--     for i=1,4 do -- Fill second plot
+--         PlaceSeed()
+--         NextLeft()
+--         PlaceSeed()
+--         NextRight()
+--         print("Refilling Slot 1...")
+--         Refill()
+--         print("Continuing...")
+--     end
+--     print("Finished, returning to start point.")
+--     Return2()
+--     print("Done!")
+-- end
+
+local function init() -- Prepare for Replanting.
     print("Initializing...")
     os.sleep(0.5)
     print("Getting in position...")
@@ -89,7 +162,7 @@ function init() -- Prepare for Replanting.
     Refill()
 end
 
-function Refill() -- Go through all slots, refilling Slot 1.
+local function Refill() -- Go through all slots, refilling Slot 1.
     turtle.select(16)
     turtle.transferTo(1)
     turtle.select(15)
